62ae89dcd3c3f1aafb16e25c9fe0c71a0eefc02b
dom davies <domdaviesit@gmail.com>
2018-09-07 21:40:57 +0100

    foreach, map and filters

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // console.log(nums);
 
 // Arrow Functions
+/// /////////////////////////////////////////////
 
 // old way
 // function sayHello (name) {
@@ -10,3 +11,51 @@
 // }
 
 // sayHello('Dom')
+
+// can use const let or var
+// if only 1 line don't need {} - looks cleaner
+// if only 1 parameter you don't need ()
+// const sayHello = name => {
+//   console.log('Hello ' + name)
+// }
+
+// sayHello('Brad')
+
+// Template literals - help with concatenating use `for the string and ${parameter} each time concatenating - use jsx in react so won't use too much
+
+// Array Methods
+/// //////////////////////////////////////////
+
+// FOREACH
+
+// const fruits = ['Apples', 'Oranges', 'Grapes']
+
+// loop through it
+
+// fruits.forEach((fruit, index) => console.log(fruit))
+
+// Map //// like FOREACH but returns an array.  loop through lists
+
+// lets take s off each fruit
+
+// const fruits = ['Apples', 'Oranges', 'Grapes']
+
+// const singleFruit = fruits.map(fruit => fruit.slice(0, -1))
+
+// console.log(singleFruit)
+
+// Filter - used in state - redux (context api)
+// filter out based on a condition
+// state is immutable (can't change it) so make a copy without something eg jaqui below.
+/// ///////////////////////////////////////////
+
+// const people = [
+//   { id: 1, name: 'Larry' },
+//   { id: 2, name: 'Jaqui' },
+//   { id: 3, name: 'Suzy' }
+// ]
+
+// filter out jaqui
+
+// const people2 = people.filter(person => person.id !== 2)
+// console.log(people2)
