9a516bd4528c13d4ef8ec2e934d0d527bff9a637
dom davies <domdaviesit@gmail.com>
2018-09-07 18:32:47 +0100

    let and const, Arrow Functions

---
+++ main.js
@@ -0,0 +1,12 @@
+// const nums = [1,2,3,4];
+// nums.push(5);
+// console.log(nums);
+
+// Arrow Functions
+
+// old way
+// function sayHello (name) {
+//   console.log('Hello ' + name)
+// }
+
+// sayHello('Dom')
